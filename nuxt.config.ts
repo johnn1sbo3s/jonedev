60ee452475f51f53075d46e5803add286f2644a0
johnn1sbo3s <joaopaulosncastro@gmail.com>
2026-07-25 14:34:26 -0300

    style: reorganize app configuration in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  app: {
+    head: {
+      title: 'JoneDev',
+    },
+  },
   devtools: { enabled: false },
+  compatibilityDate: '2025-05-15',
   future: {
     compatibilityVersion: 4,
   },
